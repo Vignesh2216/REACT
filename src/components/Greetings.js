cd7bcf4e0152ce7c4046f15766c99472ebce7d2c
VIGNESH BALASUBRAMANIAN <b.s.vignesh22012006@gmail.com>
2024-08-12 12:28:50 +0530

    First Commit

---
+++ src/components/Greetings.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Greetings(props){
+    return(
+        <div>
+            Hello {props.name}
+        </div>
+    )
+}
+export default Greetings;
---
+++ src/components/Welcome.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Welcome(){
+    return(
+        <p>WELCOME</p>
+    )
+}
+export default Welcome;
